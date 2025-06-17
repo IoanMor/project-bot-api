9611dd7ba1d22dce70f045ea948b4b9be6a11c9d
IvanMorozov <i.morozov@teplotr.ru>
2025-06-17 16:57:17 +0300

    feat (scrapper/tg) Продолжаю менять код под кафка

---
+++ common/src/main/java/me/ivanmorozov/common/kafka/KafkaDataTypeKey.java
@@ -0,0 +1,2 @@
+package me.ivanmorozov.common.kafka;public class KafkaDataTypeKey {
+}
--- scrapper-api/src/main/java/me/ivanmorozov/scrapper/services/kafka/ScrapperKafkaConsumer.java
+++ scrapper-api/src/main/java/me/ivanmorozov/scrapper/services/kafka/ScrapperKafkaConsumer.java
@@ -4,20 +4,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.ivanmorozov.common.kafka.KafkaTopics;
 import me.ivanmorozov.common.kafka.MessageTypes;
-import me.ivanmorozov.common.records.ChatRecords;
 import me.ivanmorozov.common.records.KafkaRecords;
 import me.ivanmorozov.scrapper.services.db.ChatService;
+import me.ivanmorozov.scrapper.services.db.LinkService;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+
+import static me.ivanmorozov.common.kafka.KafkaDataTypeKey.EXIST_KEY;
+import static me.ivanmorozov.common.kafka.KafkaDataTypeKey.LINK_KEY;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ScrapperKafkaConsumer {
     private final ChatService chatService;
+    private final LinkService linkService;
     private final ScrapperKafkaProducer kafkaProducer;
 
 
@@ -28,13 +31,39 @@ public class ScrapperKafkaConsumer {
 
                 if (chatService.isChatExist(request.chatId())) {
                     kafkaProducer.sendResponse(request.chatId(),
-                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_CHAT, Map.of()));
+                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_CHAT_REGISTER, Map.of()));
                 } else {
                     chatService.registerChat(request.chatId());
+                    log.info("SUCCESS/: Регистрация чата {}", request.chatId());
                     kafkaProducer.sendResponse(
                             request.chatId(),
                             new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.CREATED, Map.of())
                     );
+                }
+            }
+
+            case MessageTypes.LINK_SUBSCRIBE -> {
+
+                if (request.data() instanceof Map<?,?> dataMap && dataMap.get("Link") instanceof String link) {
+                   link = (String) dataMap.get("Link");
+                    if (linkService.isSubscribed(request.chatId(), link)){
+                        kafkaProducer.sendResponse(request.chatId(),
+                                new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_SUBSCRIBE_LINK, Map.of()));
+                    } else {
+                        linkService.subscribe(request.chatId(), link);
+                        log.info("SUCCESS/: {} Оформлена подписка на {}", request.chatId(), link);
+                        kafkaProducer.sendResponse(request.chatId(),
+                                new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.SUCCESS, Map.of(LINK_KEY, link)));
+                    }
+                }
+            }
+            case MessageTypes.CHAT_CHECK -> {
+                if (chatService.isChatExist(request.chatId())){
+                    kafkaProducer.sendResponse(request.chatId(),
+                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_CHAT_CHECK,Map.of(EXIST_KEY, true)));
+                } else {
+                    kafkaProducer.sendResponse(request.chatId(),
+                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_CHAT_CHECK,Map.of(EXIST_KEY, false)));
                 }
             }
         }
