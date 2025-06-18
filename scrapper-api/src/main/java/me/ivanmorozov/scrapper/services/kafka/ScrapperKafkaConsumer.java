0d96ceee346a68cf51ffd62f7ba3aa763250ef23
IvanMorozov <i.morozov@teplotr.ru>
2025-06-18 17:01:06 +0300

    kafka - Осталось переделать методы для акций

--- scrapper-api/src/main/java/me/ivanmorozov/scrapper/services/kafka/ScrapperKafkaConsumer.java
+++ scrapper-api/src/main/java/me/ivanmorozov/scrapper/services/kafka/ScrapperKafkaConsumer.java
@@ -7,13 +7,15 @@ import me.ivanmorozov.common.kafka.MessageTypes;
 import me.ivanmorozov.common.records.KafkaRecords;
 import me.ivanmorozov.scrapper.services.db.ChatService;
 import me.ivanmorozov.scrapper.services.db.LinkService;
+import me.ivanmorozov.scrapper.services.db.StockService;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Set;
 
-import static me.ivanmorozov.common.kafka.KafkaDataTypeKey.EXIST_KEY;
-import static me.ivanmorozov.common.kafka.KafkaDataTypeKey.LINK_KEY;
+import static me.ivanmorozov.common.kafka.KafkaDataTypeKey.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +23,7 @@ import static me.ivanmorozov.common.kafka.KafkaDataTypeKey.LINK_KEY;
 public class ScrapperKafkaConsumer {
     private final ChatService chatService;
     private final LinkService linkService;
+    private final StockService stockService;
     private final ScrapperKafkaProducer kafkaProducer;
 
 
@@ -42,11 +45,20 @@ public class ScrapperKafkaConsumer {
                 }
             }
 
+            case MessageTypes.CHAT_CHECK -> {
+                if (chatService.isChatExist(request.chatId())) {
+                    kafkaProducer.sendResponse(request.chatId(),
+                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_CHAT_CHECK, Map.of(EXIST_KEY, true)));
+                } else {
+                    kafkaProducer.sendResponse(request.chatId(),
+                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_CHAT_CHECK, Map.of(EXIST_KEY, false)));
+                }
+            }
             case MessageTypes.LINK_SUBSCRIBE -> {
 
-                if (request.data() instanceof Map<?,?> dataMap && dataMap.get("Link") instanceof String link) {
-                   link = (String) dataMap.get("Link");
-                    if (linkService.isSubscribed(request.chatId(), link)){
+                if (request.data() instanceof Map<?, ?> dataMap && dataMap.get(LINK_KEY) instanceof String link) {
+                    link = (String) dataMap.get(LINK_KEY);
+                    if (linkService.isSubscribed(request.chatId(), link)) {
                         kafkaProducer.sendResponse(request.chatId(),
                                 new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_SUBSCRIBE_LINK, Map.of()));
                     } else {
@@ -57,13 +69,32 @@ public class ScrapperKafkaConsumer {
                     }
                 }
             }
-            case MessageTypes.CHAT_CHECK -> {
-                if (chatService.isChatExist(request.chatId())){
-                    kafkaProducer.sendResponse(request.chatId(),
-                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_CHAT_CHECK,Map.of(EXIST_KEY, true)));
-                } else {
-                    kafkaProducer.sendResponse(request.chatId(),
-                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.EXIST_CHAT_CHECK,Map.of(EXIST_KEY, false)));
+            case MessageTypes.LINK_UNSUBSCRIBE -> {
+                if (request.data() instanceof Map<?, ?> dataMap && dataMap.get(LINK_KEY) instanceof String link) {
+                    boolean isUnsubscribed = linkService.unSubscribe(request.chatId(), link);
+                    kafkaProducer.sendResponse(
+                            request.chatId(),
+                            new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.OPERATION_RESULT, Map.of(LINK_KEY, isUnsubscribed)
+                            )
+                    );
+                }
+            }
+
+            case MessageTypes.LINK_GET_ALL_SUBS -> {
+                Set<String> links = linkService.getAllSubscribeLinks(request.chatId());
+                kafkaProducer.sendResponse(request.chatId(),
+                        new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.GET_ALL_LINKS,Map.of(LINK_KEY,links)));
+            }
+            case MessageTypes.STOCK_SUBSCRIBE -> {
+                if (request.data() instanceof Map<?,?> dataMap && dataMap.get(STOCK_KEY) instanceof String ticker){
+                    if (!stockService.isTickerSubscribed(request.chatId(), ticker)) {
+                        stockService.subscribe(request.chatId(), ticker);
+                        kafkaProducer.sendResponse(request.chatId(),
+                                new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.ACCEPTED, Map.of(STOCK_KEY, true)));
+                    } else {
+                        kafkaProducer.sendResponse(request.chatId(),
+                                new KafkaRecords.KafkaResponse(request.chatId(), MessageTypes.ACCEPTED, Map.of(STOCK_KEY, false)));
+                    }
                 }
             }
         }
---
+++ telegram-bot/src/main/java/me/ivanmorozov/telegrambot/util/RegistrationCache.java
@@ -0,0 +1,2 @@
+package me.ivanmorozov.telegrambot.util;public class RigistrationCache {
+}
